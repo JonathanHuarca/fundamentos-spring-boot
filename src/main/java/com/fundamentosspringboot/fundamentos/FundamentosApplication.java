6a590fa12f21ba6e398a544a5f80acf841c1eda0
JonathanHuarca <jonathan.huarca@tecsup.edu.pe>
2021-12-05 19:58:37 -0500

    Segundo commit

--- src/main/java/com/fundamentosspringboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosspringboot/fundamentos/FundamentosApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication {
-
+ /*
+ * Hola a todos
+ * */
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
